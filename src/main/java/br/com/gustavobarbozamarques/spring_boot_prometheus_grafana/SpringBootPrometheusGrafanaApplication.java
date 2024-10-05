7767fbc3f0ba3d05f20df716dc903b7fc651433d
gustavobarbozamarques <gustavomarques.contato@gmail.com>
2024-10-05 10:20:04 -0300

    Initial commit

---
+++ src/main/java/br/com/gustavobarbozamarques/spring_boot_prometheus_grafana/SpringBootPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavobarbozamarques.spring_boot_prometheus_grafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusGrafanaApplication.class, args);
+	}
+
+}
